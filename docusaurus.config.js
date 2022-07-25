a937b5056c6ee0659d5ebff2931958f2b4650376
huynd <ngohuy99bn@gmail.com>
2022-07-25 22:50:44 +0700

    cm

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,8 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Hello Everyone',
-  titleDelimiter: '^',
-
+  titleDelimiter: '|',
   tagline: 'Dinosaurs are cool',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
